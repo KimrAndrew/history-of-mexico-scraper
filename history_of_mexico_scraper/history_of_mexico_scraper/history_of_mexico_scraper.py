163b8966de81a44757bf65749da49169475f312c
KimrAndrew <andrewrk1997@gmail.com>
2021-12-21 19:35:56 -0800

    added funcitons get_citations_needed_count and get_citations_needed_report

--- history_of_mexico_scraper/history_of_mexico_scraper/history_of_mexico_scraper.py
+++ history_of_mexico_scraper/history_of_mexico_scraper/history_of_mexico_scraper.py
@@ -3,10 +3,34 @@ URL = 'https://en.wikipedia.org/wiki/History_of_Mexico'
 
 from bs4 import BeautifulSoup
 
-page = requests.get(URL)
-#print(page.content)
-soup = BeautifulSoup(page.content,'html.parser')
-#print(soup.prettify())
 
-results = soup.find_all(title='Wikipedia:Citation needed')
-print(len(results))
+def get_citations_needed(url):
+    page = requests.get(url)
+    #print(page.content)
+    soup = BeautifulSoup(page.content,'html.parser')
+    #print(soup.prettify())
+
+
+    results = soup.find_all(title='Wikipedia:Citation needed')
+    return len(results)
+#list(results)
+#print(type(results[0]))
+
+def get_citations_needed_report(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content,'html.parser')
+
+    results = soup.find_all(title='Wikipedia:Citation needed')
+    report = ''
+    for i,result in enumerate(results,1):
+        # print(i)
+        # print('\n')
+        # print(result.parent.parent.parent.text)
+        report += '\n'
+        report += result.parent.parent.parent.text
+    report += '\n'
+    return report
+
+if __name__ == '__main__':
+    print(f'Citations Needed: {get_citations_needed(URL)}')
+    print(get_citations_needed_report(URL))
